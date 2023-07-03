95b7611b0e4b8b2515f00253cdd5af1c85fba87e
Edmond Shen <egsmond@gmail.com>
2023-07-03 23:13:16 +1200

    fix: center content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center p-24">
-      <div className="flex w-full max-w-5xl items-center justify-center font-mono text-sm">
+      <div className="flex w-full items-center justify-center font-mono text-sm">
         <h1>Edmond Shen</h1>
       </div>
     </main>
