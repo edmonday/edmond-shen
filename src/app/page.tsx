e5bacaaad15d670fa12e61f96615380c6ec273f8
Edmond Shen <egsmond@gmail.com>
2023-07-04 23:12:13 +1200

    feat: update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center p-24">
       <div className="flex w-full items-center justify-center font-mono text-sm">
-        <h1>Edmond Shen</h1>
+        <h1>coming soon...</h1>
       </div>
     </main>
   )
